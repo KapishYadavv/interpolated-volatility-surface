09bb25976da80a3db31550ecd1691f4d7d40ac62
DaDerpyBoi <101107609+Pokieiscool@users.noreply.github.com>
2024-04-07 13:17:54 +0530

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,18 @@
+import os
+import tkinter
+import customtkinter
+from customtkinter import *
+
+#Framework
+win = CTk(fg_color="black")
+win.title("MenuUI v1.0")
+win.geometry("500x700")
+win.resizable(False,False)
+win.maxsize(height=1000,width=1600)
+win.minsize(height=500,width=800)
+
+#Menu
+
+
+#loop
+win.mainloop()
